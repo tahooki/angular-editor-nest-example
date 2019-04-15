eb8ee3ebdad5a3adaf85a04c837c66969d3f49fa
tahooki <tahooki12@gmail.com>
2019-04-15 22:10:09 +0900

    chore: nestjs image upload

--- nest/src/app.controller.ts
+++ nest/src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('get/content')
+  getContent(): string {
+    return JSON.stringify({content: this.appService.getContent()});
+  }
+
+  @Post('update/content')
+  updateContent(@Body() body): string {
+    this.appService.setContent(body.content);
+    return JSON.stringify({isSucceed: true});
+  }
+
+  @Post('upload')
+  @UseInterceptors(FilesInterceptor('file'))
+  uploadFile(@UploadedFiles() files) {
+    return JSON.stringify({
+      imageUrl: `http://localhost:3000/images/${files[0].filename}`,
+    });
   }
 }
--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [],
+  imports: [
+    MulterModule.register({
+      dest: './images',
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
